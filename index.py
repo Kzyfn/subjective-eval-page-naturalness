2462f75ca8e88555c75cbaa8cce6d3ed14f82cb9
Kzyfn <tomakata.y.0611@gmail.com>
2021-02-05 10:13:05 +0900

    AとBをランダムに入れ替える

--- index.py
+++ index.py
@@ -28,45 +28,71 @@ bcps = ['OSAKA864_{}.wav'.format(int2str(i*13+2)) for i in range(67)]
 # ここからウェブアプリケーション用のルーティングを記述
 # index にアクセスしたときの処理
 
-
+model_a = '/static/wav/decision_tree_nc2/'
+model_b = '/static/wav/decision_tree_wo_accent/'
 
 @app.route('/', methods=['GET'])
 def index():
     """
     ここでランダムにbcpを20個選んできて20個とも同じ画面に表示
     """
-
-    chosen_indices = np.random.randint(0, 67, 5)
+    n = 5
+    chosen_indices = np.random.randint(0, 67, n)
     chosen_bcps = []
     for idx in chosen_indices:
         chosen_bcps.append(bcps[idx])
+    
+    model_a_order = []
+    model_b_order = []
+
+    model_a_style = []
+    model_b_style = []
+    for i in range(n):
+        if np.random.rand() > 0.5:
+            style_a = "flex-direction:column;align-items:center;order:0;"
+            style_b = "flex-direction:column;align-items:center;order:1;"
+
+            model_a_order.append(1)
+            model_b_order.append(2)
+            model_a_style.append(style_a)
+            model_b_style.append(style_b)
+        else:
+            style_a = "flex-direction:column;align-items:center;order:1;"
+            style_b = "flex-direction:column;align-items:center;order:0;"
+
+            model_b_order.append(2)
+            model_a_order.append(1)
+            model_a_style.append(style_a)
+            model_b_style.append(style_b)
 
 
     random.shuffle(chosen_bcps)
-    model_a = '/static/wav/decision_tree_nc2/'
-    model_b = '/static/wav/decision_tree_wo_accent/'
     # index.html をレンダリングする
 
-    return render_template('index.html', wav_files=chosen_bcps, model_a=model_a, model_b=model_b, )
+
+    return render_template('index.html', model_a=model_a, model_b=model_b, wav_files_and_orders=zip(chosen_bcps, model_a_order, model_b_order, model_a_style, model_b_style) )
 
 
 @app.route('/done', methods=['GET', 'POST'])
 def done():
     if request.method == 'POST':
         result_path = randomname(20)#結果ファイルの識別子
-        result_dict = {model:{} for model in models}#結果格納用辞書
+        result_dict = {'file':[], 'preferred_model':[]}
 
         """
         request.form: {'/static/wav/{model}/{bcp}.wav': {値},... }みたいなものが入ってる 
         ex. {'/static/wav/model1/OSAKA1200_0001.wav': 0, '/static/wav/model2/OSAKA1200_0001.wav': 4, ...}
         """
+        models = [model_a, model_b]
+
 
         for key in request.form.keys():
-            for model in models:
-                if model in key:
-                   result_dict[model][key[key.rfind('/')+1:]] =  request.form[key]
+            result_dict['preferred_model'].append( models[int(request.form[key])] )
+            result_dict['file'].append( key )
 
-        pd.DataFrame(result_dict).to_csv('static/results/{}.csv'.format(result_path))
+
+        pd.DataFrame(result_dict).to_csv('static/results/{}.csv'.format(result_path), index=None)
+        
 
         return render_template('done.html', result_path = result_path)
     else:
